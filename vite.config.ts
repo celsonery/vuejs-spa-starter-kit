d46b559bde696cc8a868bdef4aa550097171e5df
Celso Nery <celso.nery@gmail.com>
2026-04-06 09:54:10 -0300

    feat: Add Shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'node:path'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
