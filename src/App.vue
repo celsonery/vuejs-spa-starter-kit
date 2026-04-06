4465c269b7d07072e9c7396d8a2fcb56b86e39f4
Celso Nery <celso.nery@gmail.com>
2026-04-06 09:51:02 -0300

    feat: Add DarkMode

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import { useDark } from '@vueuse/core'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 
+const isDark = useDark({
+  selector: 'html',
+  attribute: 'class',
+  valueDark: 'dark',
+  valueLight: '',
+})
 </script>
 
 <template>
-
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+      <slot />
+    </main>
+  </SidebarProvider>
 </template>
+
